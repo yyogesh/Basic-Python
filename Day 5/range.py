ecb7ea1d591e5b325acd4545995ea443585ebf2c
yyogesh <yogeshggn1@gmail.com>
2020-09-21 06:47:31 +0530

    Range function

---
+++ Day 5/range.py
@@ -0,0 +1,25 @@
+for num in range(-2, 5, 1):
+    print(num, end=", ")
+#-2, -1, 0, 1, 2, 3, 4,
+
+
+print("Displaying a range of numbers by reverse order")
+for i in range(5, -1, -1):
+    print(i, end=', ')
+
+
+# Displaying a range of numbers by reverse order
+# 5, 4, 3, 2, 1, 0
+
+
+print("Printing reverse range using reversed()")
+for i in reversed(range(0, 5)):
+    print(i)
+
+
+# Printing reverse range using reversed()
+# 4
+# 3
+# 2
+# 1
+# 0
